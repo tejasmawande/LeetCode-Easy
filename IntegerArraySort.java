f3ccea78bc931636abf4daa4e1271fefcd36c961
lenovo <lenovo@DESKTOP-BL4VNL3>
2021-02-10 16:38:34 +0530

    [Tejas]:Tried with Comparator interface

--- IntegerArraySort.java
+++ IntegerArraySort.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
-public class IntegerArraySort {
+
+public class IntegerArraySort implements Comparator<Integer>{
 
 	
 	public static void main(String[] args) {
@@ -10,11 +13,19 @@ public class IntegerArraySort {
 		
 		ArrayList<Integer> res = new ArrayList<>();
 		int[] integer1= {4,6,2,1,3};
+		List<Integer> integer2 = new ArrayList<>();
+		List<Integer> integer3 = new ArrayList<>();
+		for(Integer s: integer1) {
+			integer2.add(s);
+		}
+		 Collections.sort(integer2);
 		
+		 System.out.println("Integer2 ---"+integer2);
 		int[] ans = sortedList(integer1);
 		for(Integer s : ans) {
 			res.add(s);
 		}
+		
 		System.out.println(res);
 	}
 	
@@ -32,5 +43,10 @@ public class IntegerArraySort {
 			}
 		}
 		return list;
+	}
+
+	@Override
+	public int compare(Integer o1,Integer o2) {
+		return o2.compareTo(o1);
 	}
 }
